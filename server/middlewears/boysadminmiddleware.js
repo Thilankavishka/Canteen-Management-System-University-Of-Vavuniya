e952706e6bdacae9af0bef3ee99bd26dc40ce078
Thilan_Kavishka <thilankavishka25@gmail.com>
2024-06-21 15:59:58 +0530

    Create Middlewares

---
+++ server/middlewears/boysadminmiddleware.js
@@ -0,0 +1,28 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = async (req, res, next) => {
+  try {
+    const token = req.headers["authorization"].split(" ")[1];
+    jwt.verify(
+      token,
+      process.env.JWT_SECRET_BOYSCANTEEN_ADMIN,
+      (err, decode) => {
+        if (err) {
+          return res
+            .status(401)
+            .send({ success: false, message: "Un Authorized Boys Admin" });
+        } else {
+          req.body.id = decode.id;
+          next();
+        }
+      }
+    );
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Please Provide Auth Token",
+      error,
+    });
+  }
+};
---
+++ server/middlewears/bsadminmiddleware.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = async (req, res, next) => {
+  try {
+    const token = req.headers["authorization"].split(" ")[1];
+    jwt.verify(token, process.env.JWT_SECRET_BS_ADMIN, (err, decode) => {
+      if (err) {
+        return res
+          .status(401)
+          .send({ success: false, message: "Un Authorized Bs Admin" });
+      } else {
+        req.body.id = decode.id;
+        next();
+      }
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Please Provide Auth Token",
+      error,
+    });
+  }
+};
